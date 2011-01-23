e775fba0ec460b0f78223d7c427f18442e40ce79
James Hollingworth <jamiehollingworth@gmail.com>
2011-01-23 11:39:32 +0000

    added basic project

---
+++ Twitterr.Tasks/TwitterTasks.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using TweetSharp.Twitter.Fluent;
+
+namespace Twitterr.Tasks
+{
+    public class TwitterTasks
+    {
+        public IAsyncResult Tweet(string status)
+        {
+            return Twitter.Statuses().Update(status).BeginRequest();
+        }
+
+        private static IFluentTwitter Twitter
+        {
+            get
+            {
+                return FluentTwitter.CreateRequest()
+                    .AuthenticateWith(Settings.ConsumerKey, Settings.ConsumerSecret, Settings.Token, Settings.TokenSecret);
+            }
+        }
+    }
+}
